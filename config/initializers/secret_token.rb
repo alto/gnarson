28592fa8fce3a6f8b741755ef5b2b67eac68c417
Thorsten Böttger <boettger@mt7.de>
2012-07-14 15:27:37 +1200

    initialized a basic Rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gnarson::Application.config.secret_token = '8b8570f568b6a211c310c45698bf0f8ac5b8fae00e67b69f7e06b0010a7a6e9f20036d5b8e63a525d94101f130f628b40b667838dd6369ce1aac1fc2d5ac798f'

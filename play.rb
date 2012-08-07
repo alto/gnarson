afd3d1dc7a2661b11e7ee7164e793c40882f6759
Thorsten Böttger <boettger@mt7.de>
2012-08-07 22:19:59 +1200

    fetching the data of one day

--- play.rb
+++ play.rb
@@ -5,38 +5,48 @@ require 'redis'
 
 redis = Redis.new
 
-gz = open("http://data.githubarchive.org/2012-07-26-1.json.gz")
-js = Zlib::GzipReader.new(gz).read
+24.times.each do |hour|
+  puts "processing hour #{hour}"
+  gz = open("http://data.githubarchive.org/2012-07-26-#{hour}.json.gz")
+  js = Zlib::GzipReader.new(gz).read
+  puts "  fetched file"
 
-i = 0; stop_at = 10000
+  i = 0; stop_at = 10000
 
-types = {}
-repos = {}
-watches = {}
+  types = {}
+  repos = {}
+  watches = {}
 
-Yajl::Parser.parse(js) do |event|
-  break if i > stop_at
+  Yajl::Parser.parse(js) do |event|
+    # break if i > stop_at
 
-  # puts event.inspect
+    # puts event.inspect
 
-  # count types
-  redis.incr("github:types:#{event['type']}:2012-07-16")
+    # count processed items
+    redis.incr("github:items:2012-07-16")
+    redis.incr("github:items:2012-07-16-#{hour}")
 
-  # analyse PushEvents
-  if event['type'] == 'PushEvent'
-    key = "#{event['repository']['owner']}/#{event['repository']['name']}"
-    redis.incr("github:repos:#{key}:2012-07-16")
+    # count types
+    redis.incr("github:types:#{event['type']}:2012-07-16")
+
+    # analyse PushEvents
+    if event['type'] == 'PushEvent'
+      key = "#{event['repository']['owner']}/#{event['repository']['name']}"
+      redis.incr("github:repos:#{key}:2012-07-16")
+    end
+
+    # analyse WatchEvents
+    if event['type'] == 'WatchEvent'
+      key = "#{event['repository']['owner']}/#{event['repository']['name']}"
+      redis.incr("github:watches:#{key}:2012-07-16")
+    end
+
+    i += 1
   end
-
-  # analyse WatchEvents
-  if event['type'] == 'WatchEvent'
-    key = "#{event['repository']['owner']}/#{event['repository']['name']}"
-    redis.incr("github:watches:#{key}:2012-07-16")
-  end
-
-  i += 1
 end
 
-redis.keys("github:*").each do |key|
-  puts "#{redis.get(key)} x #{key}"
-end
+# redis.keys("github:*").each do |key|
+#   puts "#{redis.get(key)} x #{key}"
+# end
+
+redis.keys("github:items*").each {|key| puts "#{redis.get(key)} x #{key}"}; nil
